42ec5f0c5ea3c4e966dde4066e0447ae32c44b27
Gerald Emory <geraldemory@Geralds-MacBook-Pro.local>
2017-08-22 16:38:56 -0400

    added a test.js

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// pass argument of app into router function
+routes(app);
 
-
-
-
-
-
+// for the test suite; avoids app from running on port 3000 if you've loaded app into test suite
+if(require.main === 'module'){
+  app.listen(3000);
+}
 
 app.listen(3000);
